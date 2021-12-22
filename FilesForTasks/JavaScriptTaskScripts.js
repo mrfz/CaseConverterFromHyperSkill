97e42e6f92210df0603e21fd3808247c59457125
mrfz <zlobin.f@gmail.com>
2021-12-22 18:03:46 +0300

    update JavaScriptTaskScripts.js

--- FilesForTasks/JavaScriptTaskScripts.js
+++ FilesForTasks/JavaScriptTaskScripts.js
@@ -1,3 +1,13 @@
 console.log("X X O\nO X O\nO O X");
 console.log("How\ndeep\n\nis\nthe\nocean?");
 console.log("I\nlove\nwinter");
+
+/* console.log("one");
+ console.log("One");
+ console.log("two");
+console.log("two.");
+console.log("three"); */
+console.log("four");
+/* console.log("four.");
+ console.log("Four");*/
+ document.getElementById(navBar).addEventListener('click'
