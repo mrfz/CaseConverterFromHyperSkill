93f3324bc76f7bc517d27fa8dbe33d7e7f287cfb
mrfz <zlobin.f@gmail.com>
2021-12-24 18:58:04 +0300

    Last stage completed

--- assets/eventHandlers.js
+++ assets/eventHandlers.js
@@ -3,6 +3,7 @@ let UpperButton = document.getElementById("upper-case");
 let LowerButton = document.getElementById("lower-case");
 let ProperButton = document.getElementById("proper-case");
 let SentenceButton = document.getElementById("sentence-case");
+let SaveTextButton = document.getElementById("save-text-file");
 
 UpperButton.addEventListener("click", function() {
     TextArea.value = TextArea.value.toUpperCase();
@@ -24,6 +25,23 @@ SentenceButton.addEventListener("click",function() {
     TextArea.value = SentenceArray.join(". ");
 });
 
+SaveTextButton.addEventListener("click", function() {
+    download("text.txt",TextArea.value);
+});
+
 function CapitaliseFirstLetter (word) {
     return (word.slice(0,1).toUpperCase() + word.slice(1,word.length).toLowerCase());
 };
+
+function download(filename, text) {
+    let element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
